b6348cd1e53730d1743bdadbbc4b9bd93be8ed38
danidev89 <danielaruquipamendez@gmail.com>
2025-06-15 22:50:14 -0400

    titulo

--- src/secciones/Titulos.jsx
+++ src/secciones/Titulos.jsx
@@ -10,7 +10,7 @@ const Titulos = () => {
       <div className='hero-overlay' />
       <div className='hero-content text-neutral-content text-center'>
         <div className='max-w-md'>
-          <h1 className='mb-5 text-4xl font-bold'>Acadedmia de Baile Sergio el Bailador</h1>
+          <h1 className='mb-5 text-4xl font-bold'>Academia de Baile Sergio el Bailador</h1>
           <p className='mb-5'>
             Daniel Rodrigo Aruquipa Mendez
           </p>
